dbf8914a3c136a9c3f71f1eff6b2aca1387663b8
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-10-04 20:44:19 +0330

    Task-CalculateMetrics: Feat - CalculateMetricsService added

--- src/main/java/org/example/application/service/CalculateMetricsService.java
+++ src/main/java/org/example/application/service/CalculateMetricsService.java
@@ -1,11 +1,36 @@
 package org.example.application.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.adapter.out.FileWriter;
+import org.example.adapter.out.MetricPersister;
+import org.example.application.domain.Click;
+import org.example.application.domain.Impression;
+import org.example.application.domain.Metrics;
 import org.example.application.port.in.CalculateMetricsUseCase;
+
+import java.util.List;
 
 public class CalculateMetricsService implements CalculateMetricsUseCase {
 
     @Override
     public void calculateMetrics(Param param) {
-        System.out.println("Ok");
+        MetricPersister persister = new MetricPersister();
+        FileWriter writer = new FileWriter();
+        List<Click> clicks = null;
+        List<Impression> impressions = null;
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            clicks = objectMapper.readValue(param.getClicks().toString(), new TypeReference<>(){});
+            impressions = objectMapper.readValue(param.getImpressions().toString(), new TypeReference<>(){});
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        persister.saveAllClicks(clicks);
+        persister.saveAllImpressions (impressions);
+        List<Metrics> metrics = persister.calculate();
+        writer.write(metrics);
     }
 }
